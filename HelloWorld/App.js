72393af8550a0ffd115d7d46a71297e1f443ae02
itsMallix <amandamaulana00@gmail.com>
2024-01-01 18:45:16 +0700

    docs : add test text

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -5,7 +5,8 @@ export default function App() {
     <View style={{flex: 1, backgroundColor: "skyblue", padding: 10}}>
       <View style={{height: 100, width: 100, backgroundColor: "powderblue"}}></View>
       <View style={{height: 100, width: 100, backgroundColor: "red"}}></View>
-      <Text style={{fontSize: 30, color: "white"}}>Testis Badag</Text>
+      <Text style={{fontSize: 30, color: "white"}}>Test</Text>
+      <Text style={{fontSize: 30, color: "white"}}>Lorem ipsum</Text>
     </View>
   );
 }
